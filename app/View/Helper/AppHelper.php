bfd58b35ad61fdfcb9f314b91ce330aa9318dba5
rcsv <rcsv@users.noreply.github.com>
2014-05-12 08:58:22 +0900

    Create AppHelper.php

---
+++ app/View/Helper/AppHelper.php
@@ -0,0 +1,13 @@
+<?php
+App::uses('Helper', 'View');
+
+/**
+ * Application helper
+ *
+ * Add your application-wide methods in class below, your helpers
+ * will inherit them.
+ *
+ * @package   app.View.Helper
+ */
+class AppHelper extends Helper {
+}
