a7f402304d10f19bc1d7d90f3f64db38f32be427
rcsv <rcsv@users.noreply.github.com>
2014-05-12 15:35:26 +0900

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -6,4 +6,15 @@ App::uses('Profile', 'Model');
  * Users Controller
  */
 class UsersController extends AppController {
+	// methods
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('register', 'activate', 'reactivate');
+	}
+
+	public function isAuthorized($user) {
+		if(in_array($this->action, array())) {
+		}
+		return parent::isAuthorized($user);  
+	}
 }
