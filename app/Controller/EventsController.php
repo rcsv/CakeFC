ce07dad88fa201ba4b26bbf4bb4278bf4654f4c3
rcsv <rcsv@users.noreply.github.com>
2014-05-12 15:25:34 +0900

    add change method

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -32,7 +32,7 @@ class EventsController extends AppController {
 			if($this->Event->save()) {
 				$event = $this->Event->find('first', array(
 					'conditions' => array(
-						'Event.id' => $this->Event->id)));
+						'Event.' . $this->Event->primaryKey => $this->Event->id)));
 
 				$this->set(compact('event'));
 				$this->layout = 'ajax';
@@ -55,7 +55,30 @@ class EventsController extends AppController {
 	}
 	
 	public function change($id = null, $start, $end, $allDay = 'false') {
+		$this->Event->id = $id;
+		if(!$this->Event->exists()) {
+			throw new NotFoundException(__('Invalid %s', __('Event')));
+		}
 		
+		// Event.id = $id
+		$options = array(
+			'conditions' => array(
+				'Event.' . $this->Event->primaryKey => $id));
+		
+		$this->Event->unbindModel(array('belongsTo' => array('User')));
+		$event = $this->Event->find('first', $options);
+		
+		$event['Event']['start'] = date('Y-m-d H:i:s', $start);
+		$event['Event']['end'] = date('Y-m-d H:i:s', $end);
+		$event['Event']['allday'] = (strcmp($allDay, 'true') == 0) ? 1 : 0 ;
+		$event['Event']['modified'] = null;
+		
+		$this->autoLayout = false;
+		$this->autoRender = false;
+		if($this->Event->save($event)) {
+			return true;
+		}
+		return false;
 	}
 
 	// methods override
